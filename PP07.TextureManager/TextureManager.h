1647d38d01bedb612fdb74251f4d43c3d274ec6b
shinseona <20171189@game.hoseo.edu>
2018-10-11 18:19:35 +0900

    과제1

---
+++ PP07.TextureManager/TextureManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include "SDL_image.h"
+class TextureManager
+{
+public:
+	TextureManager() {}
+	~TextureManager() {}
+
+	bool load(std::string fileName, std::string id, SDL_Renderer* pRenderer);
+
+	void draw(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame2,
+		SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+	void drawFrame(std::string id, int x, int y, int width, int height,
+		int currentRow, int currentFrame,
+		SDL_Renderer* pRenderer, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+private:
+
+	std::map<std::string, SDL_Texture*> m_textureMap;
+}; 
