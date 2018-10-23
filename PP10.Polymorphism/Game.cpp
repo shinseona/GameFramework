98304707ec02750e596a3dd10cf32e6f1399e6b6
qkrtmdgns23 <qkrtmdgns23@gmail.com>
2018-10-23 13:16:26 +0900

    1

---
+++ PP10.Polymorphism/Game.cpp
@@ -0,0 +1,44 @@
+#include "texturemanager.h"
+#include<iostream>
+#include "Game.h"
+
+bool Game::init(const char* title, int xpos, int ypos,
+	int width, int height, bool fullscreen)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+
+		m_bRunning = true;
+
+		// load 부분 대치   
+		if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+		{
+			return false;
+		}
+		//m_go.load(100, 100, 128, 82, "animate");
+		//m_player.load(300, 300, 128, 82, "animate");
+
+		// init() 추가
+		m_go = new GameObject();
+		m_player = new Player();
+		m_enemy = new Enemy();
+
+		m_go->load(100, 100, 128, 82, "animate");
+		m_player->load(300, 300, 128, 82, "animate");
+		m_enemy->load(0, 0, 128, 82, "animate");
+
+		m_gameObjects.push_back(m_go);
+		m_gameObjects.push_back(m_player);
+		m_gameObjects.push_back(m_enemy);
+
+	}
+	else {
+		return false; // sdl could not initialize
+	}
+	return true;
+}
